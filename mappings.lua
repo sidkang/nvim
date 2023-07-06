9af9f1a55b9c1d8f3fb4255ae9d4c88e52b186ad
sidkang <sidskang@gmail.com>
2023-07-06 17:03:31 +0800

    minor

--- mappings.lua
+++ mappings.lua
@@ -4,43 +4,47 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    -- ["<leader>bD"] = {
-    --   function()
-    --     require("astronvim.utils.status").heirline.buffer_picker(
-    --       function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-    --     )
-    --   end,
-    --   desc = "Pick to close",
-    -- },
+	-- first key is the mode
+	n = {
+		-- second key is the lefthand side of the map
+		-- mappings seen under group name "Buffer"
+		-- ["<leader>bD"] = {
+		--   function()
+		--     require("astronvim.utils.status").heirline.buffer_picker(
+		--       function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+		--     )
+		--   end,
+		--   desc = "Pick to close",
+		-- },
 
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+		-- tables with the `name` key will be registered with which-key if it's installed
+		-- this is useful for naming menus
+		["<leader>b"] = { name = "Buffers" },
+		-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
-    ["<leader>fB"] = {
-      "<cmd>Telescope current_buffer_fuzzy_find fuzzy=false case_mode=ignore_case<cr>",
-      name = "Search current buffer",
-    },
+		-- Rust Tools
+		["<leader>rr"] = { "<Cmd>RustRunnables<CR>" },
+		["<leader>rm"] = { "<Cmd>RustExpandMacro<CR>" },
 
-    -- move lines
-    ["<A-j>"] = { ":m .+1<CR>==" },
-    ["<A-k>"] = { ":m .-2<CR>==" },
-  },
-  i = {
-    ["<A-j>"] = { "<Esc>:m .+1<CR>==gi" },
-    ["<A-k>"] = { "<Esc>:m .-2<CR>==gi" },
-  },
-  v = {
-    ["<A-j>"] = { ":m '>+1<CR>gv=gv" },
-    ["<A-k>"] = { ":m '<-2<CR>gv=gv" },
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+		["<leader>fB"] = {
+			"<Cmd>Telescope current_buffer_fuzzy_find fuzzy=false case_mode=ignore_case<CR>",
+			name = "Search current buffer",
+		},
+
+		-- move lines
+		["<A-j>"] = { ":m .+1<CR>==" },
+		["<A-k>"] = { ":m .-2<CR>==" },
+	},
+	i = {
+		["<A-j>"] = { "<Esc>:m .+1<CR>==gi" },
+		["<A-k>"] = { "<Esc>:m .-2<CR>==gi" },
+	},
+	v = {
+		["<A-j>"] = { ":m '>+1<CR>gv=gv" },
+		["<A-k>"] = { ":m '<-2<CR>gv=gv" },
+	},
+	t = {
+		-- setting a mapping to false will disable it
+		-- ["<esc>"] = false,
+	},
 }
